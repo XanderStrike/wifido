8e4318bb388459190e89dc97e5a2b9191b1e00a7
HParker <adamhess1991@gmail.com>
2013-10-10 10:39:05 -0700

    put in functions. Not yet tested.

--- sniffer/xgpsd.py
+++ sniffer/xgpsd.py
@@ -2,35 +2,32 @@ import bluetooth
 import time
 # import threading
 
-def pair:
+sock = None
+gps = {'lat': None, 'long':None}
 
-def start_listening:
-
-def get_fix:
-
-
-if __name__ == '__main__':
-
-    # what we know about the bluetooth device
-    target_name = 'XGPS150-365060'
-    target_address = "00:19:01:36:50:60"
-    print "Searching for device..."
+def pair(target_address=None, target_name=None):
     # if there is not a address given it will search for one.
     while target_address == None:
         near_devices = bluetooth.discover_devices()
         for near_address in near_devices:
             if target_name == bluetooth.lookup_name( near_address ):
                 target_address = near_address
+                # print "Discovered!", target_address
                 break
+    try:
+        sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+        port = 1
+        sock.connect((target_address,port))
+        return true
+    except:
+        return false
+    
 
-    print "Discovered!", target_address
 
-    sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-    port = 1
-    sock.connect((target_address,port))
+
+def start_listening():
     data = ""
     old_data = ""
-
     while True: # make thread...
         time.sleep(1)
         data = sock.recv(1024)
@@ -41,10 +38,18 @@ if __name__ == '__main__':
                 if line.find("\r\n") != -1:
                     line = line.strip()
                     line_values = line.split(',')
-                    if line_values[0] = '$GPRMC':
-                        gps['lat']: line_values[3] + line_values[4]
-                        gps['long']: line_values[5] + line_values[5]
+                    if line_values[0] == '$GPRMC':
+                        gps['lat'] = line_values[3] + line_values[4]
+                        gps['long'] = line_values[5] + line_values[5]
                     print line # put in hash here...
                     old_data = ""
                 else:
                     olddata = line
+
+def get_fix():
+    return gps
+
+if __name__ == '__main__':
+    if pair('00:19:01:36:50:60'):
+        start_listening
+    get_fix
