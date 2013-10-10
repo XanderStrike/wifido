0173b883a0537242247b59f5d11ad9403cf02d8f
nolanblew <nolanblew@hotmail.com>
2013-10-10 11:41:19 -0700

    Working on page for search

---
+++ sampledatabase.sql
@@ -0,0 +1,3 @@
+CREATE TABLE wifis(time integer, mac text, essid text, strength real, lat real, long real, alt real);
+
+INSERT INTO wifis (time, mac, essid, strength, lat, long, alt) VALUES (100, "AA:BB:CC:DD:EE", "Westmont Encrypted", 70, 38, -129, 134);
