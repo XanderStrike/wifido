79cd2ab36b9b45d68f2f40cc7f9d9b54bbb6ecc4
HParker <ahess@westmont.edu>
2013-10-31 01:26:57 -0700

    working gps

--- sniffer/xgpsd.py
+++ sniffer/xgpsd.py
@@ -1,50 +1,65 @@
-import bluetooth
+# Adam Hess 2013
+# gpsdx: listens to a gps unit and reports the result.
+
+import os
+from gps import *
+from time import *
 import time
-# import threading
+import threading
 
-def pair:
+gpsd = None
 
-def start_listening:
+class Listener(threading.Thread):
+  def __init__(self):
+    # make a thread
+    threading.Thread.__init__(self)
+    # start the gps listening
+    global gpsd
+    gpsd = gps(mode=WATCH_ENABLE)
+    self.current_value = None
+    self.running = True
 
-def get_fix:
+  def run(self):
+    # keep getting the newest data
+    global gpsd
+    while self.running: # thread prevents it from hanging.
+      gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
 
+def start_listening():
+    # start listening
+    gpsd = Listener()
+    try:
+        gpsd.start()
+        print "Now Listening..."
+    except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+        print "\nKilling Thread..."
+        gpsd.running = False
+        gpsd.join()
+    print "Done.\nExiting."
+
+
+def get_data():
+    # hash of common gps data
+    print "hey hey hey"
+    print gpsd.fix.latitude
+    return {
+        'lat': gpsd.fix.latitude,
+        'lon': gpsd.fix.longitude,
+        'alt': gpsd.fix.altitude,
+        'speed': gpsd.fix.speed, # m/s
+        'sats': gpsd.satellites,
+        'utc': gpsd.utc,
+        'time': gpsd.fix.time
+    }
+
+
+def stop_listening():
+    gpsd.running = False
+    gpsd.join()
+    print "Done"
 
 if __name__ == '__main__':
-
-    # what we know about the bluetooth device
-    target_name = 'XGPS150-365060'
-    target_address = "00:19:01:36:50:60"
-    print "Searching for device..."
-    # if there is not a address given it will search for one.
-    while target_address == None:
-        near_devices = bluetooth.discover_devices()
-        for near_address in near_devices:
-            if target_name == bluetooth.lookup_name( near_address ):
-                target_address = near_address
-                break
-
-    print "Discovered!", target_address
-
-    sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-    port = 1
-    sock.connect((target_address,port))
-    data = ""
-    old_data = ""
-
-    while True: # make thread...
-        time.sleep(1)
-        data = sock.recv(1024)
-        if len(data) > 0:
-            data = old_data + data
-            lines = data.splitlines(1)
-            for line in lines:
-                if line.find("\r\n") != -1:
-                    line = line.strip()
-                    line_values = line.split(',')
-                    if line_values[0] = '$GPRMC':
-                        gps['lat']: line_values[3] + line_values[4]
-                        gps['long']: line_values[5] + line_values[5]
-                    print line # put in hash here...
-                    old_data = ""
-                else:
-                    olddata = line
+    start_listening()
+    while True:
+        time.sleep(5) #set to whatever
+        print get_data()
