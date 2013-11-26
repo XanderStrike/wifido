4beae4616e533a404a9266c5a141f706c53e4ca5
xanderstrike <xanderstrike@gmail.com>
2013-11-26 11:33:19 -0800

    Upload database to librarycounter; use wlan0 for both processes

--- sniffer/wifido.py
+++ sniffer/wifido.py
@@ -6,14 +6,16 @@ import RPi.GPIO as GPIO
 import Tweet
 import json
 import sqlite3 as lite
+from datetime import datetime
 from pprint import pprint
 
 logging.basicConfig()
 log = logging.getLogger("wifido")
 log.setLevel(logging.DEBUG)
 
-interface = "wlan1"  # Use a second wireless device
+interface = "wlan0"  # Use a second wireless device
 
+last_update_day = -1
 
  # How long to wait for a given signal strength (between 0 and 1)
 def wait_time (signal):
@@ -21,6 +23,8 @@ def wait_time (signal):
 
 
 if __name__ == "__main__":
+
+    os.system("ifdown wlan0")
 
     con = lite.connect('db/db.sqlite3')
 
@@ -33,47 +37,58 @@ if __name__ == "__main__":
 
     xgpsd.start_listening()
 
+    last_coords = [0,0]
+
     while True:
         # Get GPS data
         gpsdata = xgpsd.get_data()
+        
+        distance_moved = (abs(gpsdata["lat"] - last_coords[0]) + abs(gpsdata["lon"] - last_coords[1]))
 
-        # Process WiFi data
-        strongest_signal = 0
-        for cell in IWList(interface).getData().values():
+        if distance_moved < .0001: 
+            # Process WiFi data
+            strongest_signal = 0
+            for cell in IWList(interface).getData().values():
+                match = re.match(r"(\d+)/(\d+)", cell["Signal"])
+                strength_nu = match.group(1)
+                strength_de = match.group(2)
+                strength = float(strength_nu) / float(strength_de)
+                essid = cell["ESSID"]
+                mac = cell["MAC"]
 
-            match = re.match(r"(\d+)/(\d+)", cell["Signal"])
-            strength_nu = match.group(1)
-            strength_de = match.group(2)
-            strength = float(strength_nu) / float(strength_de)
-            essid = cell["ESSID"]
-            mac = cell["MAC"]
+                if essid == "":
+                    continue
 
-            if essid == "":
-                continue
+                # Keep track of the strength of our primary network.
+                if essid == "Westmont_Encrypted" and strength > strongest_signal:
+                    strongest_signal = strength
 
-            # Keep track of the strength of our primary network.
-            if essid == "Westmont_Encrypted" and strength > strongest_signal:
-                strongest_signal = strength
+                Tweet.bark(essid, json_data)
 
-            Tweet.bark(essid, json_data)
+                # Write to db
+                values = [
+                          str(gpsdata["time"]),
+                          str(mac),
+                          str(essid),
+                          str(strength),
+                          str(gpsdata["lat"]),
+                          str(gpsdata["lon"]),
+                          str(gpsdata["alt"])
+                         ]
 
-            # Write to db
-            values = [
-                      str(gpsdata["time"]),
-                      str(mac),
-                      str(essid),
-                      str(strength),
-                      str(gpsdata["lat"]),
-                      str(gpsdata["lon"]),
-                      str(gpsdata["alt"])
-                     ]
-
-            cur = con.cursor()
-            cur.execute("INSERT INTO wifis values('" + "','".join(values) + "')")
-            
-
-            
-        con.commit()
+                cur = con.cursor()
+                cur.execute("INSERT INTO wifis values('" + "','".join(values) + "')")
+            con.commit()
+        else if datetime.now().hour == 12 and last_upload_day = datetime.now().day:
+            last_upload_day = datetime.now().day
+            os.system("ifup wlan0")
+            loop_count = 0
+            while loop_count < 10 and not re.search("inet addr:\d+\.\d+\.\d+\.\d+", os.popen("ifconfig wlan0")):
+                time.sleep(5)
+                loop_count += 1
+            os.system("scp db/db.sqlite3 librarycounter:wifido/server/db/data.sqlite3")
+            Tweet.commit()
+            os.system("ifdown wlan0")
 
         print "Current signal strength: " + str(strongest_signal)
         print "Location: " + str(gpsdata["lat"]) + " " + str(gpsdata["lon"]) + " " + str(gpsdata["alt"])
@@ -83,6 +98,9 @@ if __name__ == "__main__":
         time.sleep(.1)
         GPIO.output(11, True)
 
+        # Get last gps coords
+        last_coords = [gpsdata["lat"], gpsdata["lon"]]
+        
         # Sleep while playing sound
         proc = subprocess.Popen(['mpg321', '-q', 'beep.mp3'], shell=False)
         time.sleep(wait_time(strongest_signal))
